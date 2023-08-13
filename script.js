6e73292b56f27e77ad0bf83f63a906241675508d
Jabez Villamarzo <simplesimmering@gmail.com>
2023-08-13 21:14:22 +0800

    Make gameResults work with mixed case answers

--- script.js
+++ script.js
@@ -11,38 +11,41 @@ function getComputerChoice() {
     }
 }
 
-function gameResults(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return "The player and the computer is in a draw. Both got " + playerSelection;
-    }
-
+function gameResults(playerSelection, computerSelection) { 
     let winMessage = "You win! ";
     let loseMessage = "You lose! ";
-
+    
     let rockPaperMessage = "Paper beats Rock!";
     let paperScissorsMessage = "Scissors beats Paper!";
     let scissorsRockMessage = "Rock beats Scissors!";
 
-    switch(playerSelection) {
-        case "Rock":
-            switch(computerSelection) {
-                case "Paper":
+    let loweredPlayerInput = playerSelection.toLowerCase(); 
+    let loweredComputerInput = computerSelection.toLowerCase(); 
+    
+    if (loweredPlayerInput === loweredComputerInput) {
+        return "The player and the computer is in a draw. Both got " + playerSelection;
+    }    
+
+    switch(loweredPlayerInput) {
+        case "Rock".toLowerCase():
+            switch(loweredComputerInput) {
+                case "Paper".toLowerCase():
                     return loseMessage + rockPaperMessage;
                 default:
                     return winMessage + scissorsRockMessage;
             }
 
-        case "Paper":
-            switch(computerSelection) {
-                case "Rock":
+        case "Paper".toLowerCase():
+            switch(loweredComputerInput) {
+                case "Rock".toLowerCase():
                     return winMessage + rockPaperMessage;
                 default:
                     return loseMessage + paperScissorsMessage;
             }
 
         default:
-            switch(computerSelection) {
-                case "Rock":
+            switch(loweredComputerInput) {
+                case "Rock".toLowerCase():
                     return loseMessage + scissorsRockMessage;
                 default:
                     return winMessage + paperScissorsMessage;
