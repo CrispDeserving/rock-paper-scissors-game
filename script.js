0ee5206d8fa037b817f3f603324fbd8163b4f19f
Jabez Villamarzo <simplesimmering@gmail.com>
2023-08-13 20:55:26 +0800

    Add gameResults function with one player case

--- script.js
+++ script.js
@@ -10,3 +10,26 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
+
+function gameResults(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "The player and the computer is in a draw. Both got" + playerSelection;
+    }
+
+    let winMessage = "You win! ";
+    let loseMessage = "You lose! ";
+
+    let rockPaperMessage = "Paper beats Rock!";
+    let paperScissorsMessage = "Scissors beats Paper!";
+    let scissorsRockMessage = "Rock beats Scissors!";
+
+    switch(playerSelection) {
+        case "Rock":
+            switch(computerSelection) {
+                case "Paper":
+                    return loseMessage + rockPaperMessage;
+                default:
+                    return winMessage + scissorsRockMessage;
+            }
+    }
+}
